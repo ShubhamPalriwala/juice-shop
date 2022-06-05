9e72fb1af6d8d925497b30d99be693bdc2d54425
ShubhamPalriwala <spalriwalau@gmail.com>
2022-06-05 14:25:52 +0530

    feat: directAccess spec

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -15,8 +15,34 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-export = (on, config) => {
+
+import * as Config from "config";
+import { Product } from "../../data/types";
+
+export default (on, config) => {
+  on("task", {
+    GetBlueprint() {
+      for (const product of Config.get<Product[]>("products")) {
+        if (product.fileForRetrieveBlueprintChallenge) {
+          let blueprint = product.fileForRetrieveBlueprintChallenge;
+          return blueprint;
+        }
+      }
+    },
+    toISO8601() {
+      // This is a copy of the utils.toISO8601(date) function but since it has an indirect dependency of gracfully, it is unable to import the function in a browser hence we natively use the code here.
+      let date = new Date();
+      let day = "" + date.getDate();
+      let month = "" + (date.getMonth() + 1);
+      const year = date.getFullYear();
+
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+
+      let updatedDate = [year, month, day].join("-");
+      return updatedDate;
+    },
+  });
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
